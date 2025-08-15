63db72fe9b3f7ea1827ffa27e7e8b4f1319930f4
swdevsmz <duke2per@gmail.com>
2025-08-15 14:16:33 +0900

    Install expo-router

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import { JSX } from 'react';
+import { View, Text } from 'react-native';
+
+const Index = (): JSX.Element => {
+    return (
+        <View>
+            <Text>Memo App</Text>
+        </View>
+    );
+};
+
+export default Index;
