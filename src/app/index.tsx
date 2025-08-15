4b741815c5bf7c2d2c2ca12089b5a664c45cf24d
swdevsmz <duke2per@gmail.com>
2025-08-15 14:28:53 +0900

    index.ts structured

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,60 @@
-import { JSX } from 'react';
-import { View, Text } from 'react-native';
+import { JSX } from "react";
+import { View, Text, StyleSheet } from "react-native";
 
 const Index = (): JSX.Element => {
-    return (
+  return (
+    <View style={styles.container}>
+      {/* ヘッダー */}
+      <View>
         <View>
-            <Text>Memo App</Text>
+          <Text>Memo App</Text>
+          <Text>ログアウト</Text>
         </View>
-    );
+      </View>
+      
+      {/* メモリスト */}
+      <View>
+        <View>
+          <Text>買い物リスト</Text>
+          <Text>2025/8/1 10:00</Text>
+        </View>
+        <View>
+          <Text>X</Text>
+        </View>
+
+        <View>
+          <Text>買い物リスト</Text>
+          <Text>2025/8/1 10:00</Text>
+        </View>
+        <View>
+          <Text>X</Text>
+        </View>
+
+        <View>
+          <Text>買い物リスト</Text>
+          <Text>2025/8/1 10:00</Text>
+        </View>
+        <View>
+          <Text>X</Text>
+        </View>
+      </View>
+
+      {/* フッター */}
+      <View>
+        <Text>+</Text>
+      </View>
+
+    </View>
+  );
 };
 
 export default Index;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+
+});
