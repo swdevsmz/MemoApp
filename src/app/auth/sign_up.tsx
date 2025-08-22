d518977b40f1d64940af44000923ba2b518eb520
swdevsmz <duke2per@gmail.com>
2025-08-22 12:16:19 +0900

    useStateの追加

--- src/app/auth/sign_up.tsx
+++ src/app/auth/sign_up.tsx
@@ -1,4 +1,4 @@
-import { JSX } from 'react';
+import { JSX, useState } from 'react';
 import { View, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
 
 import Button from '../../components/Button';
@@ -10,12 +10,30 @@ const handlePress = (): void => {
 }
 
 const SignUp = (): JSX.Element => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     return (
         <View style={styles.container}>
             <View style={styles.inner}>
                 <Text style={styles.title}>Sign Up</Text>
-                <TextInput style={styles.input} value='Email address' />
-                <TextInput style={styles.input} value='Password' />
+                <TextInput
+                    style={styles.input}
+                    value={email}
+                    onChangeText={(text) => { setEmail(text) }}
+                    autoCapitalize="none"
+                    keyboardType="email-address"
+                    placeholder="Email Address"
+                    textContentType="emailAddress"
+                />
+                <TextInput
+                    style={styles.input}
+                    value={password}
+                    onChangeText={(text) => { setPassword(text) }}
+                    autoCapitalize="none"
+                    secureTextEntry={true}
+                    placeholder="Password"
+                    textContentType="password"
+                />
                 <Button label='submit' onPress={handlePress} />
                 <View style={styles.footer}>
                     <Text style={styles.footerText}>Already registered?</Text>
