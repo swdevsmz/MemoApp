f2fac811175e01cefecf72a646e77661d65bb4c9
swdevsmz <duke2per@gmail.com>
2025-08-22 16:43:00 +0900

    編集画面の実装

--- src/app/memo/detail.tsx
+++ src/app/memo/detail.tsx
@@ -9,17 +9,17 @@ import { onSnapshot, doc } from "firebase/firestore";
 import { auth, db } from '../../config';
 import { type Memo } from '../../../types/memo';
 
-const handlePress = (): void => {
-    router.push('/memo/edit');
+const handlePress = (id: string): void => {
+    router.push({ pathname: '/memo/edit', params: { id: id } });
 }
 
 const Detail = (): JSX.Element => {
-    const { id } = useLocalSearchParams();
+    const id = String(useLocalSearchParams().id);
 
     const [memo, setMemo] = useState<Memo | null>(null);
-    useEffect(() => {
+    const unsubscribe = useEffect(() => {
         if (auth.currentUser === null) { return };
-        const ref = doc(db, `users/${ auth.currentUser.uid }/memos/`, String(id));
+        const ref = doc(db, `users/${ auth.currentUser.uid }/memos/`, id);
         const unsubscribe = onSnapshot(ref, (memoDoc) => {
             setMemo(memoDoc.data() as Memo);
         })
@@ -37,7 +37,7 @@ const Detail = (): JSX.Element => {
                     {memo?.bodyText}
                 </Text>
             </ScrollView>
-            <CircleButton onPress={handlePress} style={{ top: 60, bottom: 'auto' }}>
+            <CircleButton onPress={() => { handlePress(id) }} style={{ top: 60, bottom: 'auto' }}>
                 <Icon name='pencil' size={40} color='#ffffff' />
             </CircleButton>
         </View>
--- src/app/memo/edit.tsx
+++ src/app/memo/edit.tsx
@@ -1,22 +1,61 @@
-import { JSX } from 'react';
+import { JSX, useEffect, useState } from 'react';
 import {
-    View, TextInput, StyleSheet, KeyboardAvoidingView
+    View, TextInput, StyleSheet, KeyboardAvoidingView,
+    Alert
 } from 'react-native';
 import CircleButton from '../../components/CircleButton';
 import Icon from '../../components/Icon';
-import { router } from 'expo-router';
+import { router, useLocalSearchParams } from 'expo-router';
+import { doc, setDoc, Timestamp, getDoc } from 'firebase/firestore';
+import { auth, db } from '../../config';
 
-const handlePress = (): void => {
-    router.back();
+const handlePress = (id: string, bodyText: string): void => {
+    if (auth.currentUser === null) { return };
+    const ref = doc(db, `users/${ auth.currentUser.uid }/memos/`, id);
+    setDoc(ref, {
+        bodyText,
+        updatedAt: Timestamp.fromDate(new Date()),
+    }
+    )
+        .then(() => {
+            router.back()
+        })
+        .catch((error) => {
+            Alert.alert('更新に失敗しました。')
+        });
 }
 
 const Edit = (): JSX.Element => {
+    const id = String(useLocalSearchParams().id);
+    console.log(id);
+
+    const [bodyText, setBodyText] = useState('');
+    useEffect(() => {
+        if (auth.currentUser === null) { return };
+        const ref = doc(db, `users/${ auth.currentUser.uid }/memos/`, id);
+        getDoc(ref)
+            .then((docRef) => {
+                const remoteBodyText = docRef?.data()?.bodyText;
+                setBodyText(remoteBodyText);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
+
+
     return (
         <KeyboardAvoidingView behavior='height' style={styles.container}>
             <View style={styles.inputContainer}>
-                <TextInput multiline style={styles.input} value={'買い物\nリスト'}></TextInput>
+                <TextInput
+                    multiline
+                    style={styles.input}
+                    value={bodyText}
+                    onChangeText={(text) => { setBodyText(text) }}
+
+                ></TextInput>
             </View>
-            <CircleButton onPress={handlePress}>
+            <CircleButton onPress={() => handlePress(id, bodyText)}>
                 <Icon name='check' size={40} color='#fff' />
             </CircleButton>
         </KeyboardAvoidingView>
@@ -28,8 +67,6 @@ const styles = StyleSheet.create({
         flex: 1,
     },
     inputContainer: {
-        paddingVertical: 32,
-        paddingHorizontal: 27,
         flex: 1,
     },
     input: {
@@ -37,6 +74,8 @@ const styles = StyleSheet.create({
         textAlignVertical: 'top',
         fontSize: 16,
         lineHeight: 24,
+        paddingVertical: 32,
+        paddingHorizontal: 27,
     },
 });
 
