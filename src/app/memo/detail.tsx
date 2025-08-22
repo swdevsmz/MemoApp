a04ab208cf35a5ad0e5a3fbfdba7c4adf93ca355
swdevsmz <duke2per@gmail.com>
2025-08-22 16:11:49 +0900

    メモの詳細画面の実行

--- src/app/memo/detail.tsx
+++ src/app/memo/detail.tsx
@@ -1,9 +1,13 @@
-import { JSX } from "react";
+import { JSX, useState, useEffect } from "react";
 import { View, Text, ScrollView, StyleSheet } from "react-native";
 
 import CircleButton from "../../components/CircleButton";
 import Icon from "../../components/Icon";
 import { router, useLocalSearchParams } from "expo-router";
+
+import { onSnapshot, doc } from "firebase/firestore";
+import { auth, db } from '../../config';
+import { type Memo } from '../../../types/memo';
 
 const handlePress = (): void => {
     router.push('/memo/edit');
@@ -12,19 +16,25 @@ const handlePress = (): void => {
 const Detail = (): JSX.Element => {
     const { id } = useLocalSearchParams();
 
-
+    const [memo, setMemo] = useState<Memo | null>(null);
+    useEffect(() => {
+        if (auth.currentUser === null) { return };
+        const ref = doc(db, `users/${ auth.currentUser.uid }/memos/`, String(id));
+        const unsubscribe = onSnapshot(ref, (memoDoc) => {
+            setMemo(memoDoc.data() as Memo);
+        })
+        return unsubscribe;
+    }, []);
 
     return (
         <View style={styles.container}>
             <View style={styles.memoHeader}>
-                <Text style={styles.memoTitle}>買い物リスト</Text>
-                <Text style={styles.memoDate}>2025年8月1日 10:00</Text>
+                <Text numberOfLines={1} style={styles.memoTitle}>{memo?.bodyText}</Text>
+                <Text style={styles.memoDate}>{memo?.updatedAt?.toDate().toLocaleDateString('ja-JP')}</Text>
             </View>
             <ScrollView style={styles.memoBody}>
                 <Text style={styles.memoBodyText}>
-                    メモの内容ああああああああああああああ
-                    いいいいいいいいいいいいいいいいいい
-                    うううううううううううううううううう
+                    {memo?.bodyText}
                 </Text>
             </ScrollView>
             <CircleButton onPress={handlePress} style={{ top: 60, bottom: 'auto' }}>
@@ -58,10 +68,10 @@ const styles = StyleSheet.create({
         lineHeight: 16,
     },
     memoBody: {
-        paddingVertical: 32,
         paddingHorizontal: 27,
     },
     memoBodyText: {
+        paddingVertical: 32,
         fontSize: 16,
         lineHeight: 24,
         color: "#000000", // メモ本文の文字色（黒）
